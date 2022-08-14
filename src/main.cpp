a813d2287f2e4256c2939cbb666bfbd9bccd28ca
SameetAsadullah <sameetassadullah744@gmail.com>
2022-08-15 02:57:07 +0500

    Complete code with working screenshots

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    cout << "Name: Sameet Asadullah" << endl;
+    cout << "Roll Number: i180479" << endl;
+    cout << "Section: D" << endl;
+    cout << "Semester No # 6" << endl;
+    cout << "Subject: PDC" << endl;
+    cout << "Class Task # 3" << endl;
+    cout << "Status: Task Completed Successfully" << endl;
+    return 0;
+}
